73ef5c480897f0e531be8ce632de5ddcbe212075
Martin Guido <guidomartin7@gmail.com>
2023-03-28 20:35:42 -0300

    first commit

---
+++ src/test/java/com/bdd/TP/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdd.TP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
