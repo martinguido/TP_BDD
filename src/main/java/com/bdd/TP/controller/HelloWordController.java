06d1013134b9de53bc65107bd0faf65982277d77
FelicitasKlix <felikpc@gmail>
2023-04-09 17:00:42 -0300

    First commit Feli

--- src/main/java/com/bdd/TP/controller/HelloWordController.java
+++ src/main/java/com/bdd/TP/controller/HelloWordController.java
@@ -1,11 +1,20 @@
 package com.bdd.TP.controller;
 
 import com.bdd.TP.dto.HelloWordResponse;
+import com.bdd.TP.service.CammesaService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("api/v1")
 public class HelloWordController {
+    private final CammesaService cammesaService;
+
+    public HelloWordController(CammesaService cammesaService) {
+        this.cammesaService = cammesaService;
+    }
+
     @GetMapping("/hello")
     public HelloWordResponse helloWord(@RequestParam(value = "name", defaultValue = "Word") String name){
         return new HelloWordResponse("Hello " + name + "!");
@@ -15,4 +24,15 @@ public class HelloWordController {
     public String helloName(@PathVariable("name") String name){
         return "Hello " + name +"!";
     }
+
+    @GetMapping("/cammesa/esFeriado")
+    public String esFeriado(@RequestParam(value="fecha") String fecha){
+        return cammesaService.esFeriado(fecha);
+    }
+
+    @GetMapping("/cammesa/demandaFeriadoMasCercano")
+    public String demandaFeriadoMasCercano(@RequestParam(value="fecha") String fecha){
+        return cammesaService.demandaFeriadoMasCercano(fecha);
+    }
+
 }
---
+++ src/main/java/com/bdd/TP/service/CammesaService.java
@@ -0,0 +1,39 @@
+package com.bdd.TP.service;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Service
+public class CammesaService {
+    private RestTemplateBuilder restTemplateBuilder;
+    //es una clase que te permite hacer requests a servicios
+
+    CammesaService(RestTemplateBuilder restTemplateBuilder){
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
+    public String esFeriado(String fecha){
+        return restTemplateBuilder
+                .build()
+                .getForObject("https://api.cammesa.com/demanda-svc/demanda/EsDiaFeriado?fecha=" + fecha, String.class);
+    }
+
+    public String demandaFeriadoMasCercano(String newDate){
+        int i = 1;
+        //String newDate = date;
+        while(Objects.equals(esFeriado(newDate), "false")){
+            newDate = LocalDate.parse(newDate).plusDays(i).toString();
+            i = i+1;
+        }
+
+        return newDate;
+    }
+
+}
