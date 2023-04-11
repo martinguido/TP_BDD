7b467bbfadc65ee81680b6cb16ae326163b73fbd
Martin Guido <guidomartin7@gmail.com>
2023-04-10 22:29:28 -0300

    add postgre

--- src/main/java/com/bdd/TP/repository/RegionRepository.java
+++ src/main/java/com/bdd/TP/repository/RegionRepository.java
@@ -1,9 +1,9 @@
 package com.bdd.TP.repository;
 
 import com.bdd.TP.dao.Region;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RegionRepository extends CrudRepository<Region, Long> {
+public interface RegionRepository extends JpaRepository<Region, Long> {
 }
